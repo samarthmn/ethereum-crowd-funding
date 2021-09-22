832c9dddcd06d0d6d43e90759914e65ea7b81403
Samarth MN <samarth.m.n@gmail.com>
2021-09-22 12:42:20 +0530

    added FE

---
+++ front-end/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Fund Labs</title>
+        <meta name="description" content="Future of crowd funding" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
+
+export default Home;
